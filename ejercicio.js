2774508cef0f80cd2050fc5d2d42309a4ffb6892
Miguel <migue2801224@gmail.com>
2024-11-13 20:41:14 -0500

    primer comentario

---
+++ ejercicio.js
@@ -0,0 +1,10 @@
+//Hash Table algoritmo de Agenda Telefonica
+//#1 creamos la hashtable
+class HashTable {
+    constructor(tamaño) {
+        this.table = new Array(tamaño);
+        this.size = tamaño;
+    }
+
+    
+}
